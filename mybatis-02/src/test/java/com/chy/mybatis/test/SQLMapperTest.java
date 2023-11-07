bbcf7f3544bdb7f8860dca09a614adf2c6a4ba10
EziKoo <1050864544@qq.com>
2023-11-07 14:54:13 +0800

    MyBatis处理模糊查询。

---
+++ mybatis-02/src/test/java/com/chy/mybatis/test/SQLMapperTest.java
@@ -0,0 +1,20 @@
+package com.chy.mybatis.test;
+
+import com.chy.mybatis.mapper.SQLMapper;
+import com.chy.mybatis.pojo.User;
+import com.chy.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SQLMapperTest {
+
+    @Test
+    public void testGetUserByLike(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> list = mapper.getUserByLike("chy");
+        System.out.println(list);
+    }
+}
