8ed3337296025241fbe6412ebdbdcb9f7e88346a
EziKoo <1050864544@qq.com>
2023-11-13 11:26:19 +0800

    MyBatis的二级缓存。

--- mybatis-03/src/test/java/com/chy/mybatis/test/CacheMapperTest.java
+++ mybatis-03/src/test/java/com/chy/mybatis/test/CacheMapperTest.java
@@ -3,8 +3,14 @@ package com.chy.mybatis.test;
 import com.chy.mybatis.mapper.CacheMapper;
 import com.chy.mybatis.pojo.Emp;
 import com.chy.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class CacheMapperTest {
 
@@ -17,7 +23,7 @@ public class CacheMapperTest {
      */
 
     @Test
-    public void testCache(){
+    public void testOneCache(){
         SqlSession sqlSession1 = SqlSessionUtils.getSqlSession();
         CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
         Emp emp1 = mapper1.getEmpByEid(1);
@@ -34,6 +40,26 @@ public class CacheMapperTest {
 
         Emp emp2 = mapper1.getEmpByEid(1);
         System.out.println(emp2);
+    }
+
+
+    @Test
+    public void testTwoCache(){
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            SqlSession sqlSession1 = sqlSessionFactory.openSession(true);
+            CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
+            System.out.println(mapper1.getEmpByEid(1));
+            sqlSession1.close();
+            SqlSession sqlSession2 = sqlSessionFactory.openSession(true);
+            CacheMapper mapper2 = sqlSession2.getMapper(CacheMapper.class);
+            System.out.println(mapper2.getEmpByEid(1));
+            sqlSession2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
