7aaa00821a3c4880f6196f158f3c930c147bab30
EziKoo <1050864544@qq.com>
2023-11-13 14:16:30 +0800

    MyBatis的逆向工程之奢华尊享版。

---
+++ mybatis-04/src/test/java/com/chy/mybatis/test/MBGTest.java
@@ -0,0 +1,42 @@
+package com.chy.mybatis.test;
+
+import com.chy.mybatis.mapper.EmpMapper;
+import com.chy.mybatis.pojo.Emp;
+import com.chy.mybatis.pojo.EmpExample;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MBGTest {
+
+    @Test
+    public void testMBG(){
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+            // 查询所有数据
+            // List<Emp> list = mapper.selectByExample(null);
+            // list.forEach(emp -> System.out.println(emp));
+
+            // 根据条件查询
+            // EmpExample example = new EmpExample();
+            // example.createCriteria().andEmpNameEqualTo("李四").andAgeGreaterThanOrEqualTo(20);
+            // example.or().andDidIsNotNull();
+            // List<Emp> list = mapper.selectByExample(example);
+            // list.forEach(emp -> System.out.println(emp));
+
+            mapper.updateByPrimaryKeySelective(new Emp(1,"老六",90,null,"555@qq.com",3));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
