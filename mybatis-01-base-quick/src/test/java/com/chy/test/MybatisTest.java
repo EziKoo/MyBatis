5915416b73b452d6906c8e1d5c4303e38acd5b3c
EziKoo <1050864544@qq.com>
2023-11-22 16:42:47 +0800

    mybatis-mybatis方式的原理和实现。

--- mybatis-01-base-quick/src/test/java/com/chy/test/MybatisTest.java
+++ mybatis-01-base-quick/src/test/java/com/chy/test/MybatisTest.java
@@ -37,7 +37,11 @@ public class MybatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         // 4、获取接口的代理对象（代理技术）调用代理对象的方法，就会查找mapper接口的方法
+        // jdk动态代理技术生成的mapper代理对象
         EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        // 内部拼接接口的全限定符+方法名 去查找sql语句标签
+        // 1、拼接 类的全限定符.方法名 整合参数 -> ibatis对应的方法传入参数
+        // mybatis底层依然用的是ibatis只不过有固定的模式！
         Employee employee = mapper.queryById(1);
         System.out.println("employee = " + employee);
 
@@ -73,6 +77,9 @@ public class MybatisTest {
         Student student = sqlSession.selectOne("xx.jj.kkk", 1);
         // 缺点：1、sql语句标签对应的字符串标识，容易出现错误 2、参数需要进行整合只能传递一个！ 3、返回值问题！
         System.out.println("student = " + student);
+
+        // 解决以上问题：方法调用，你会不会出错？
+
 
         // 5、提交事务（非DQL）和释放资源
         sqlSession.close();
