3cc3563dc89eeee8fa96ce1f41a60990ce55240c
EziKoo <1050864544@qq.com>
2023-11-23 14:50:26 +0800

    mybatis-动态语句set和trim标签。

--- mybatis-06-high-dynsql/src/main/java/com/chy/mapper/EmployeeMapper.java
+++ mybatis-06-high-dynsql/src/main/java/com/chy/mapper/EmployeeMapper.java
@@ -16,4 +16,9 @@ public interface EmployeeMapper {
     // 根据员工的姓名和工资查询员工信息
     List<Employee> query(@Param("name") String name, @Param("salary") Double salary);
 
+    List<Employee> queryTrim(@Param("name") String name, @Param("salary") Double salary);
+
+    // 根据员工id更新员工的数据，我们要求 传入的name和salary不为空才更新
+    int update(Employee employee);
+
 }
