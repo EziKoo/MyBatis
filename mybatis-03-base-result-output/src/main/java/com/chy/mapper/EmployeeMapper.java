ff97e549127d601ad58e7fd4da0da538cdb10128
EziKoo <1050864544@qq.com>
2023-11-23 09:35:32 +0800

    mybatis-单个简单类型和定义别名。

---
+++ mybatis-03-base-result-output/src/main/java/com/chy/mapper/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.chy.mapper;
+
+/**
+ * @Author: EziKoo
+ * @Date: 2023/11/22 15:14
+ * @Description: 规定数据库方法即可。
+ */
+
+public interface EmployeeMapper {
+
+    // 如果是我们的DML语句(插入，修改，删除)
+    int deleteById(Integer id);
+
+    // 指定输出类型，查询语句
+    // 根据员工的id查询员工的姓名
+    String queryNameById(Integer id);
+
+    // 根据员工的id查询员工的薪资
+    Double querySalaryById(Integer id);
+
+
+}
